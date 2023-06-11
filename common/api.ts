7ade4c61d5a649ed2d81d5bdde264b359c82eeaa
Thomas Wolf <thomas@huggingface.co>
2023-06-11 21:26:29 +0200

    update api

--- common/api.ts
+++ common/api.ts
@@ -7,13 +7,9 @@ namespace api {
   export async function getRepoStargazers(
     repo: string,
     token?: string,
-    page?: number
   ) {
-    let url = `https://api.github.com/repos/${repo}/stargazers?per_page=${DEFAULT_PER_PAGE}`;
+    let url = `https://huggingface.co/api/spaces/${repo}/likers?expand=True`;
 
-    if (page !== undefined) {
-      url = `${url}&page=${page}`;
-    }
     return axios.get(url, {
       headers: {
         Accept: "application/vnd.github.v3.star+json",
@@ -23,14 +19,14 @@ namespace api {
   }
 
   export async function getRepoStargazersCount(repo: string, token?: string) {
-    const { data } = await axios.get(`https://api.github.com/repos/${repo}`, {
+    const { data } = await axios.get(`https://huggingface.co/api/spaces/${repo}`, {
       headers: {
         Accept: "application/vnd.github.v3.star+json",
         Authorization: token ? `token ${token}` : "",
       },
     });
 
-    return data.stargazers_count;
+    return data.likes;
   }
 
   export async function getRepoStarRecords(
@@ -38,71 +34,23 @@ namespace api {
     token: string,
     maxRequestAmount: number
   ) {
-    const patchRes = await getRepoStargazers(repo, token);
+    const requestPages = await getRepoStargazers(repo, token);
 
-    const headerLink = patchRes.headers["link"] || "";
+    // const resArray: string[] = []
+    // for (let i = 0; i < requestPages.data.length; ) {
+    //   resArray.push(requestPages.data[i].likedAt);
+    // }
+    const resArray: string[] = requestPages.data.map((item: any) => item.likedAt);
 
-    let pageCount = 1;
-    const regResult = /next.*&page=(\d*).*last/.exec(headerLink);
-
-    if (regResult) {
-      if (regResult[1] && Number.isInteger(Number(regResult[1]))) {
-        pageCount = Number(regResult[1]);
-      }
-    }
-
-    if (pageCount === 1 && patchRes?.data?.length === 0) {
-      throw {
-        status: patchRes.status,
-        data: [],
-      };
-    }
-
-    const requestPages: number[] = [];
-    if (pageCount < maxRequestAmount) {
-      requestPages.push(...utils.range(1, pageCount));
-    } else {
-      utils.range(1, maxRequestAmount).map((i) => {
-        requestPages.push(Math.round((i * pageCount) / maxRequestAmount) - 1);
-      });
-      if (!requestPages.includes(1)) {
-        requestPages.unshift(1);
-      }
-    }
-
-    const resArray = await Promise.all(
-      requestPages.map((page) => {
-        return getRepoStargazers(repo, token, page);
-      })
-    );
-
+    resArray.sort((a, b) => Date.parse(a) - Date.parse(b));
     const starRecordsMap: Map<string, number> = new Map();
 
-    if (requestPages.length < maxRequestAmount) {
-      const starRecordsData: {
-        starred_at: string;
-      }[] = [];
-      resArray.map((res) => {
-        const { data } = res;
-        starRecordsData.push(...data);
-      });
-      for (let i = 0; i < starRecordsData.length; ) {
-        starRecordsMap.set(
-          utils.getDateString(starRecordsData[i].starred_at),
-          i + 1
-        );
-        i += Math.floor(starRecordsData.length / maxRequestAmount) || 1;
-      }
-    } else {
-      resArray.map(({ data }, index) => {
-        if (data.length > 0) {
-          const starRecord = data[0];
-          starRecordsMap.set(
-            utils.getDateString(starRecord.starred_at),
-            DEFAULT_PER_PAGE * (requestPages[index] - 1)
-          );
-        }
-      });
+    for (let i = 0; i < resArray.length; ) {
+      starRecordsMap.set(
+        utils.getDateString(resArray[i]),
+        i + 1
+      );
+      i += Math.floor(resArray.length / maxRequestAmount) || 1;
     }
 
     const starAmount = await getRepoStargazersCount(repo, token);
@@ -128,14 +76,14 @@ namespace api {
     token?: string
   ): Promise<string> {
     const owner = repo.split("/")[0];
-    const { data } = await axios.get(`https://api.github.com/users/${owner}`, {
-      headers: {
-        Accept: "application/vnd.github.v3.star+json",
-        Authorization: token ? `token ${token}` : "",
-      },
-    });
+    // const { data } = await axios.get(`https://api.github.com/users/${owner}`, {
+    //   headers: {
+    //     Accept: "application/vnd.github.v3.star+json",
+    //     Authorization: token ? `token ${token}` : "",
+    //   },
+    // });
 
-    return data.avatar_url;
+    return ""; // data.avatar_url;
   }
 }
 
